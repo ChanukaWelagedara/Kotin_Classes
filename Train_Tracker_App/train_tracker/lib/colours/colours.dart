a414288c4f880e01cf271ad84a4dff7993809194
ChanukaWelagedara <139690214+ChanukaWelagedara@users.noreply.github.com>
2023-12-25 21:23:13 +0530

    Set The Second Screen

---
+++ Train_Tracker_App/train_tracker/lib/colours/colours.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 50, 52, 54);
+const Color secondaryColor = Color.fromARGB(255, 255, 255, 255);
+const Color shadowColor = Color.fromARGB(255, 207, 100, 100);
+
